ac11310489d4f4184b4a4c45c6a60903b5a06f45
Michel <mnemonic01@users.noreply.github.com>
2021-11-22 23:45:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 provider "oci" {
   region           = "${var.region}"
   tenancy_ocid     = "${var.tenancy_ocid}"
-  compartment_ocid  = "${var.compartment.ocid"
+  compartment_ocid  = "${var.compartment.ocid}"
   user_ocid        = "${var.user_ocid}"
   fingerprint      = "${var.fingerprint}"
   private_key_path = "${var.private_key_path}"
