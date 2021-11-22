f599344ba4dd8264769b5d1f4a6d0689637e3193
michel.schildmeijer <michel.schildmeijer@ns.nl>
2021-11-22 10:45:25 +0100

    var

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ provider "oci" {
     COMPARTMENT
 ------------------------------------------------------------------------------*/
 
-resource "oci_identity_compartment" "demo_compartment" { 
+resource "oci_identity_compartment" { 
   provider       = "oci.home"
   compartment_id = "${var.compartment_ocid}"
   description    = "${var.project_name}"
